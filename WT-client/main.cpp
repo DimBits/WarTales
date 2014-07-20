5885af7ad1954359f10c6654f4319570f4753237
Vjacheslav Bezborodov <waww@live.ru>
2014-07-20 15:07:54 +0600

    WT-client folder was created

---
+++ WT-client/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: dbits
+ *
+ * Created on July 20, 2014, 2:44 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
